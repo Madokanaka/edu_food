a2c76acbf688d8d465eec25a22485726cacb13be
Seitek <seita20078@gmail.com>
2025-05-02 21:11:51 +0600

    initial commit

---
+++ src/main/java/org/attractor/edu_food/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package org.attractor.edu_food.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Slf4j
+public class SecurityConfig {
+    private final DataSource dataSource;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .formLogin(form -> form
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/auth/login")
+//                        .successHandler((request, response, authentication) -> {
+//                            if (authentication.getAuthorities().stream()
+//                                    .anyMatch(auth -> auth.getAuthority().equalsIgnoreCase("APPLICANT"))) {
+//                                response.sendRedirect("/vacancies");
+//                            } else if (authentication.getAuthorities().stream()
+//                                    .anyMatch(auth -> auth.getAuthority().equalsIgnoreCase("EMPLOYER"))) {
+//                                response.sendRedirect("/resumes");
+//                            } else {
+//                                response.sendRedirect("/profile");
+//                            }
+//                        })
+                        .failureUrl("/auth/login?error=true")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .permitAll())
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                                .anyRequest().permitAll()
+
+//                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
+}
---
+++ src/test/java/org/attractor/edu_food/EduFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package org.attractor.edu_food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
