fec4279ac6a5b74b4adc5270d2cf33ffc81e21f1
Seitek <seita20078@gmail.com>
2025-05-05 16:40:55 +0600

    created tables and added photos for restaurants

---
+++ src/main/resources/db/changelog/initial/004_insert_photoname.sql
@@ -0,0 +1,39 @@
+UPDATE restaurants
+SET photo_name = 'italian.jpg'
+WHERE name = 'La Trattoria';
+
+UPDATE restaurants
+SET photo_name = 'japanese.jpg'
+WHERE name = 'Sushi Bar Tokyo';
+
+UPDATE restaurants
+SET photo_name = 'french.jpg'
+WHERE name = 'Bistro Parisien';
+
+UPDATE restaurants
+SET photo_name = 'steak.jpg'
+WHERE name = 'Steak House Grill';
+
+UPDATE restaurants
+SET photo_name = 'mexican.jpg'
+WHERE name = 'Taco Loco';
+
+UPDATE restaurants
+SET photo_name = 'chinese.jpg'
+WHERE name = 'Peking Duck House';
+
+UPDATE restaurants
+SET photo_name = 'vegan.jpg'
+WHERE name = 'The Vegan Corner';
+
+UPDATE restaurants
+SET photo_name = 'italian-1.jpg'
+WHERE name = 'Pasta Fresca';
+
+UPDATE restaurants
+SET photo_name = 'italian-2.jpg'
+WHERE name = 'Mamma Mia!';
+
+UPDATE restaurants
+SET photo_name = 'french-1.jpg'
+WHERE name = 'Le Petit Café';
